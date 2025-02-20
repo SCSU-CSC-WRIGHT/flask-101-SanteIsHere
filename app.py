6237b3b731dcc534dc52cd0ff9b63d004ab406c0
SanteIsHere <r.a.frye00@gmail.com>
2025-02-19 19:27:39 -0500

    Complete Exercise 1 (Setup virtual env, install requirements, run app) and Exercise 2 (Update app.py)

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+# Import the `render_template` function to render the `index.html` file.
 
 app = Flask(__name__)
 
+
+# Route to the root of the application
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template("index.html") 
 
 if __name__ == '__main__':
     app.run(debug=True)
