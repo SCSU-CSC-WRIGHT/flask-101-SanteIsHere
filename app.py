b98b98e22d58a770115ff70cea95d23dca81375f
SanteIsHere <r.a.frye00@gmail.com>
2025-02-20 20:15:57 -0500

    Add stylesheet, icons, and other aesthetic flair

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 # Import the `render_template` function to render the `index.html` file.
 
+# Set app name
 app = Flask(__name__)
 
 
-# Route to the root of the application
+# Map root URL at "/" to home() function, which renders the `index.html` page
 @app.route('/')
 def home():
     return render_template("index.html") 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Run the application in "debug" mode to get diagnostics in terminal
